eb3e227f149bd9ded4e2895ec55bf7e851702db6
Petr Schmidt <schmidt@dellos.io>
2024-05-12 15:41:20 +0200

    feat: create `AudioTransformerForm`

---
+++ src/components/audio-transformer-form.tsx
@@ -0,0 +1,140 @@
+'use client';
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import {
+  AUDIO_TRANSFORMER_FIELD_CONFIG,
+  AUDIO_TRANSFORMER_FORM_SCHEMA,
+} from '@/schemas/audio-transformer-form-schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { forwardRef, ReactNode } from 'react';
+import { FormSliderFieldItem } from '@/components/form-slider-field-item';
+import { SliderConfig } from '@/types/form';
+
+const DEFAULT_VALUES = {
+  applySlowdown: true,
+  slowdown: 0.85,
+  applyReverb: true,
+  reverbDelay: 250,
+  reverbDecayFactor: 20,
+  reverbWetDryMix: 90,
+};
+
+type InferredFormSchema = z.infer<typeof AUDIO_TRANSFORMER_FORM_SCHEMA>;
+
+type SliderFieldsDefinition = {
+  name: keyof InferredFormSchema;
+  sliderConfig: SliderConfig;
+  label: ReactNode;
+  labelAppendix?: ReactNode;
+  description?: ReactNode;
+};
+
+export const AudioTransformerForm = forwardRef<HTMLFormElement>(({}, ref) => {
+  const form = useForm<InferredFormSchema>({
+    resolver: zodResolver(AUDIO_TRANSFORMER_FORM_SCHEMA),
+    defaultValues: DEFAULT_VALUES,
+  });
+
+  return (
+    <Form {...form}>
+      <form ref={ref} className='flex flex-col gap-y-6'>
+        <FormField
+          name='file'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Audio File</FormLabel>
+              <FormControl>
+                <Input type='file' {...field} />
+              </FormControl>
+              <FormDescription />
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {SLIDER_FIELDS.map(({ name, ...sliderFieldItemProps }) => (
+          <FormField
+            key={name}
+            control={form.control}
+            name={name}
+            render={({ field }) => (
+              <FormSliderFieldItem<InferredFormSchema>
+                controllerRenderProps={field}
+                {...sliderFieldItemProps}
+              />
+            )}
+          />
+        ))}
+      </form>
+    </Form>
+  );
+});
+
+AudioTransformerForm.displayName = 'AudioTransformerForm';
+
+const SLIDER_FIELDS: SliderFieldsDefinition[] = [
+  {
+    name: 'slowdown',
+    sliderConfig: AUDIO_TRANSFORMER_FIELD_CONFIG.slowdown,
+    label: 'Slowdown Multiplier',
+    description: (
+      <>
+        This value determines the rate of slowing down a song.{' '}
+        <b>The lower the value, the slower the song.</b>
+      </>
+    ),
+  },
+  {
+    name: 'reverbDelay',
+    sliderConfig: AUDIO_TRANSFORMER_FIELD_CONFIG.reverb.delay,
+    label: 'Reverb — Delay',
+    labelAppendix: '(in milliseconds)',
+    description: (
+      <>
+        This value controls the echo and delay effects of a song.{' '}
+        <b>Higher values create a more pronounced and lingering echo.</b>
+      </>
+    ),
+  },
+  {
+    name: 'reverbDecayFactor',
+    sliderConfig: AUDIO_TRANSFORMER_FIELD_CONFIG.reverb.decayFactor,
+    label: 'Reverb — Decay Factor',
+    labelAppendix: '(in %)',
+    description: (
+      <>
+        This value field defines how long the reverb lasts.{' '}
+        <b>
+          Higher values extend the decay time, creating a longer-lasting reverb
+          effect.
+        </b>
+      </>
+    ),
+  },
+  {
+    name: 'reverbWetDryMix',
+    sliderConfig: AUDIO_TRANSFORMER_FIELD_CONFIG.reverb.wetDryMix,
+    label: 'Reverb — Wet-Dry Mix',
+    labelAppendix: '(in %)',
+    description: (
+      <>
+        The value adjusts the balance between the reverb effect and the original
+        sound.{' '}
+        <b>
+          Higher values increase the reverb effect, while lower values emphasize
+          the original sound.
+        </b>
+      </>
+    ),
+  },
+];
